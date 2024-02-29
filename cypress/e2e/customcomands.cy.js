df1f9ffca15c44309ebe1dcb7df6624832e08daa
benyfz <158505691+benyfz@users.noreply.github.com>
2024-02-29 13:13:19 +0100

    Update customcomands.cy.js

--- cypress/e2e/customcomands.cy.js
+++ cypress/e2e/customcomands.cy.js
@@ -4,7 +4,7 @@ import { login, passwd } from "../fixtures/loginData.json"
 
     describe("Custom Commands", () => {
         it("Logowanie do strony automationpractice.pl", () => {
-            cy.Login(login, passwd); // Należy wpisan parametry - "login oraz hasło" albo podłożyć dane z pliku w folderze "fixtures"
+            cy.Login(login, passwd); // Należy wpisać parametry - "login oraz hasło" albo podłożyć dane z pliku w folderze "fixtures"
         })
 
 })
